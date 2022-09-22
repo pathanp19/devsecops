45219ce34be7788f49f4e3507a3924af08a84ed9
pathanp19 <47770334+pathanp19@users.noreply.github.com>
2022-09-22 18:22:20 +0800

    updated sh file for trivy

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
+
 dockerImageName=$(awk 'NR==1 (print $2)' Dockerfile)
 echo $dockerImageName
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
-docker run --rm -v $WORKSPACE: /root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light SdockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 # Trivy scan result processing 
 exit_code=$?
-echo "Exit Code : $exit code"
+echo "Exit Code : $exit_code"
 # Check scan results 
 if [[ "$(exit_code)" == 1 ]]; then
 echo "Image scanning failed. Volnerabilities found"
