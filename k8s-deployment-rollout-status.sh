37976e3e5b90c96ce324a7f287aa8ecc8ec0643e
pathanp19 <47770334+pathanp19@users.noreply.github.com>
2022-09-24 23:21:09 +0800

    updated script rollout

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -4,11 +4,11 @@
 
 sleep 10s
 
-if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) == "deployment "devsecops" successfully rolled out" ]];
+if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]];
 then
-echo "Deployment ${deploymentName} Rollout is success"
-else
-echo "deployment ${deploymentName} Rollout has Failed"
+echo "Deployment ${deploymentName} Rollout has Failed"
 kubectl -n default rollout undo deploy ${deploymentName}
 exit 1;
+else
+echo "Deployment ${deploymentName} Rollout is success"
 fi
