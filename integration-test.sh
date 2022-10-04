a52c25b7df89fc6ec80cab269f4d4ca8716f39e3
pathanp19 <47770334+pathanp19@users.noreply.github.com>
2022-10-04 16:28:04 +0800

    test

--- integration-test.sh
+++ integration-test.sh
@@ -12,6 +12,7 @@ if [[ ! -z "$PORT" ]];
 then
 response=$(curl -s $applicationURL:$PORT$applicationURI)
 http_code=$(curl -s -o /dev/null -w "%(http _code)" $applicationURL:$PORT$applicationURI)
+
 if [[ "$response" == 100 ]];
 then
 echo "Increment Test Passed"
