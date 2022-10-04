2eb93a03115b7abd59d381831a03e645b056e5dc
pathanp19 <47770334+pathanp19@users.noreply.github.com>
2022-10-04 16:43:54 +0800

    updated script

--- integration-test.sh
+++ integration-test.sh
@@ -6,12 +6,13 @@ sleep 5s
 PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
 echo $PORT
-echo $applicationURL:$PORT/$applicationURI
+echo $applicationURL:$PORT$applicationURI
 
 if [[ ! -z "$PORT" ]];
 then
+
 response=$(curl -s $applicationURL:$PORT$applicationURI)
-http_code=$(curl -s -o /dev/null -w "%(http _code)" $applicationURL:$PORT$applicationURI)
+http_code=$(curl -s -o /dev/null -w "%{http _code}" $applicationURL:$PORT$applicationURI)
 
 if [[ "$response" == 100 ]];
 then
